3feb061539648c106df24b442162f7947181a32e
John Tuttle <thejwt@gmail.com>
2017-09-14 21:19:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -97,7 +97,13 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+    if(cardNumber != 0){
+      console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}`)
+      cart = []
+    }else{
+      console.log(`Sorry, we don't have a credit card on file for you.`)
+    }
+
 }
 
 addToCart("vanilla");
