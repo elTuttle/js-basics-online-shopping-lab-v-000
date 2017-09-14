c784ac4666d3cddf27faa88d58bab358d4057ade
John Tuttle <thejwt@gmail.com>
2017-09-14 21:25:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-    if(cardNumber === undefined){
+    if(cardNumber !== undefined){
       console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}`)
       cart = []
     }else{
